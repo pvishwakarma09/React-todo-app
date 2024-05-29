3521a6018a2079b5d412b17e6f15d3157ee0a755
PREM VISHWAKARMA <147607826+pvishwakarma09@users.noreply.github.com>
2024-05-29 12:49:55 +0530

    Text name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between bg-slate-700 text-white py-3">
       <div className="logo">
-        <span className="font-bold text-xl mx-8">My<span className="text-slate-300">Task</span></span>
+        <span className="font-bold text-xl mx-8">My<span className="text-red-500">Task</span></span>
       </div>
       <ul className="flex gap-8 mx-9">
         <li className="Cursor-pointer hover:font-bold transition-all duration-500">Home</li>
