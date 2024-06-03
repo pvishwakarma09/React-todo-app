cc28c6e0a84bbc8d2befacdd68565cfb8f0340f4
PREM VISHWAKARMA <147607826+pvishwakarma09@users.noreply.github.com>
2024-06-03 13:34:36 +0530

    file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-slate-800 text-white py-3">
-      <div className="logo flex ">
-        <img
-          className="h-8 w-8 mx-4 object-cover"
-          src="src/assets/todo.png"
-          alt="logo-image"
-        />
-        <h1 className="font-bold text-2xl hover:cursor-pointer hover:text-black ">
-          My<span className="text-red-600">Task</span>
-        </h1>
-      </div>
-    </nav>
+    <>
+      <nav className="flex bg-slate-800 text-white py-3">
+        <div className="logo flex ">
+          <img
+            className="h-8 w-8 mx-4 object-cover"
+            src="src/assets/todo.png"
+            alt="logo-image"
+          />
+          <h1 className="font-bold text-2xl hover:cursor-pointer hover:text-black ">
+            My<span className="text-red-600">Task</span>
+          </h1>
+        </div>
+      </nav>
+    </>
   );
 };
 
