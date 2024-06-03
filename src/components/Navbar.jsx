71c075ca51a60cf3926bf8e5893b66cb4b635534
PREM VISHWAKARMA <147607826+pvishwakarma09@users.noreply.github.com>
2024-06-03 19:18:53 +0530

    Add comment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <>
       <nav className="flex bg-slate-800 text-white py-3">
         <div className="logo flex ">
-          <img
+          {/* <img
             className="h-8 w-8 mx-4 object-cover"
             src="src/assets/todo.png"
             alt="logo-image"
-          />
+          /> */}
           <h1 className="font-bold text-2xl hover:cursor-pointer hover:text-black ">
             My<span className="text-red-600">Task</span>
           </h1>
